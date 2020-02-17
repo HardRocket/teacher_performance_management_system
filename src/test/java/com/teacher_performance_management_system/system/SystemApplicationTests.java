f87105a2bfdef0c49886f471a77bd6bcfc8cfb9c
HardRocket <snailzzw@163.com>
2020-02-17 10:57:09 +0800

    first commit

---
+++ src/test/java/com/teacher_performance_management_system/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacher_performance_management_system.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
