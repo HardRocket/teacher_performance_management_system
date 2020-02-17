d99dc4561c34253df8acd1d373822f13f5ede5cf
HardRocket <snailzzw@163.com>
2020-02-17 10:57:09 +0800

    first commit

---
+++ src/main/java/com/teacher_performance_management_system/system/SystemApplication.java
@@ -0,0 +1,13 @@
+package com.teacher_performance_management_system.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+
+}
